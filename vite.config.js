2b31ab05a0cef1c711919dae2358c081ac71dc85
Furzy1595 <acharyanirmal159@gmail.com>
2026-06-29 15:37:45 +0545

    v1.2

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: '/My_Portfolio/',
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? './' : '/',
   plugins: [react()],
   server: {
     port: 3000,
     open: true
   }
-})
+}))
